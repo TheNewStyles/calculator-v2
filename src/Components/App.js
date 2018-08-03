1e35d35b96cd4e5c0bfe7c88b924b2c74544e616
TheNewStyles <tyler.peterson@utah.edu>
2018-08-03 09:57:24 -0500

    add basic flexbox grid

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <div className="calc-wrapper">
+          <div className="row">
+            <div className="red"></div>
+            <div className="green"></div>
+            <div className="orange"></div>
+          </div>
+          <div className="row">
+            <div className="blue"></div>
+            <div className="pink"></div>
+            <div className="purple"></div>
+          </div>       
+        {/* // equals buttons
+        // numbers buttons
+        // operations buttons 
+        // decimal button
+        // clear button
+        // display screen */}
+        </div>
       </div>
     );
   }
