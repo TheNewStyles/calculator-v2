a50538be91e3fe8b1e4f80ac5f9005ab3c988a95
TheNewStyles <tyler.peterson@utah.edu>
2018-08-07 12:56:32 -0500

    Add reverse polish notation and yarding algorithm

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,9 +15,9 @@ class App extends Component {
     this.handleNumberClick = this.handleNumberClick.bind(this);
     this.handleParenClick = this.handleParenClick.bind(this);
     this.handleOperatorClick = this.handleOperatorClick.bind(this);
-    this.handleEqualsClick = this.handleEqualsClick.bind(this);
+    this.handleCalculateClick = this.handleCalculateClick.bind(this);
     this.handleClearClick = this.handleClearClick.bind(this);
-    this.handlePeriodClick = this.handlePeriodClick.bind(this);
+    this.handleDecimalClick = this.handleDecimalClick.bind(this);
   }    
 
   handleNumberClick(e) {
@@ -37,12 +37,14 @@ class App extends Component {
       this.setState({
         displayText: this.state.displayText + e.target.innerText
       })
-    } else if (entered === leftParen) {  
+    } else if (entered === leftParen) { 
+      // TODO: fix Operators may be next to left parens 
       if (this.isANumber(lastChar)) {
         this.setState({
           displayText: this.state.displayText + e.target.innerText
         })
       }     
+      // TODO: fix Operators may be next to left parens
     } else if (entered === rightParen) {
       //TODO match parens
       if (this.isANumber(lastChar)) {
@@ -70,7 +72,7 @@ class App extends Component {
     })
   }
 
-  handlePeriodClick(e) {
+  handleDecimalClick(e) {
     const entered = e.target.innerText;
     const lastChar = this.state.displayText.length > 0 ? this.state.displayText[this.state.displayText.length - 1] : "";
 
@@ -81,28 +83,67 @@ class App extends Component {
     }
   }
 
-  handleEqualsClick(e) {
-    // const equals = "=";  
-    //handle equals
-      // remove trailing periods/operands/parens 
-      // divide by zero = NaN 
-      // determine other calculator scenarios 
-      // shunt ladder?
-      // update display text with answer
+  handleCalculateClick(e) {
+    const postFix = this.yard(this.state.displayText);
+    const answer = this.reversePolishNotation(postFix);
+    this.setState({
+      displayText: answer
+    })
   }
 
-  // helper methods
+  //adapted from https://eddmann.com/posts/implementing-the-shunting-yard-algorithm-in-javascript/
+  yard(infix) {
+    let ops = {'+': 1, '-': 1, '*': 2, '/': 2};
+    let peek = (a) => a[a.length - 1];
+    let stack = [];
+  
+    return infix
+      .split('')
+      .reduce((output, token) => {
+        if (parseFloat(token)) {
+          output.push(token);
+        }
+  
+        if (token in ops) {
+          while (peek(stack) in ops && ops[token] <= ops[peek(stack)])
+            output.push(stack.pop());
+          stack.push(token);
+        }
+  
+        if (token == '(') {
+          stack.push(token);
+        }
+  
+        if (token == ')') {
+          while (peek(stack) != '(')
+            output.push(stack.pop());
+          stack.pop();
+        }
+  
+        return output;
+      }, [])
+      .concat(stack.reverse())
+      .join(' ');
+  };
+  
+  //adapted from https://eddmann.com/posts/small-rpn-implementation-in-javascript/
+  reversePolishNotation(ts, s = []) {
+    ts.split(' ').forEach(t =>
+      s.push(t == +t ? t : eval(s.splice(-2,1)[0] + t + s.pop())));
+    return s[0];
+  }
+
   isANumber(lastChar) {
-    const operators = ["/","X","+","-"];
-    const period = ".";
+    const operators = ["/","*","+","-"];
+    const decimal = ".";
     const leftParen = "(";
     const rightParen = ")";
     const isLastCharAnOperator = operators.indexOf(lastChar) >= 0;
-    const isLastCharAPeriod = lastChar === period;
+    const isLastCharAdecimal = lastChar === decimal;
     const isLastCharAParen = lastChar === leftParen || lastChar === rightParen;
     const isLastCharEmpty = lastChar === "";
 
-    if (!isLastCharEmpty && !isLastCharAnOperator && !isLastCharAParen && !isLastCharAPeriod) {
+    if (!isLastCharEmpty && !isLastCharAnOperator && !isLastCharAParen && !isLastCharAdecimal) {
       return true;
     }
     return false;
@@ -130,7 +171,7 @@ class App extends Component {
             <Button content="4" buttonStyles="num-button" handleClick={this.handleNumberClick} />
             <Button content="5" buttonStyles="num-button" handleClick={this.handleNumberClick} />
             <Button content="6" buttonStyles="num-button" handleClick={this.handleNumberClick} />
-            <Button content="X" buttonStyles="operator-button" handleClick={this.handleOperatorClick}  />
+            <Button content="*" buttonStyles="operator-button" handleClick={this.handleOperatorClick}  />
           </div>    
           <div className="row">
             <Button content="1" buttonStyles="num-button" handleClick={this.handleNumberClick} />
@@ -140,8 +181,8 @@ class App extends Component {
           </div>  
           <div className="row">
             <Button content="0" buttonStyles="num-button" handleClick={this.handleNumberClick} />
-            <Button content="." buttonStyles="num-button" handleClick={this.handlePeriodClick} />
-            <Button content="=" buttonStyles="equals-button" />
+            <Button content="." buttonStyles="num-button" handleClick={this.handleDecimalClick} />
+            <Button content="=" buttonStyles="equals-button" handleClick={this.handleCalculateClick} />
             <Button content="+" buttonStyles="operator-button" handleClick={this.handleOperatorClick}  />
           </div>                 
         </div>
