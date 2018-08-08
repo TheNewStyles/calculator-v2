faef1193a1c172cadea7db94ed340cdd66c26b83
TheNewStyles <tyler.peterson@utah.edu>
2018-08-08 12:18:30 -0500

    Updates

--- src/Components/KeyPressHelpers.js
+++
@@ -1,3 +0,0 @@
-export function isType(type, value) {
-  return type.indexOf(value) >= 0 ? true : false;
-}
--- src/__tests__/KeyPressHelpers.test.js
+++
@@ -1,6 +0,0 @@
-import { isType } from '../Components/KeyPressHelpers.js';
-
-it('isOperator | enter number array and number should return true', () => {
-  expect(isType(["1","2","3","4","5","6","7","8","9","0"], "1")).toBe(true);
-});
-
